d9c2cd28a4b6bea3cfd48891234eef58f4af8797
itad12.outsource <itad12.outsource@panindai-ichilife.co.id>
2019-10-21 16:30:48 +0700

    Add Plugin

---
+++ src/android/FileChooser.java
@@ -0,0 +1,100 @@
+package com.megster.cordova;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONException;
+
+public class FileChooser extends CordovaPlugin {
+
+    private static final String TAG = "FileChooser";
+    private static final String ACTION_OPEN = "open";
+    private static final String ACTION_IMAGE =  "image";
+    private static final int PICK_FILE_REQUEST = 1;
+    CallbackContext callback;
+
+    @Override
+    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
+
+        if (action.equals(ACTION_OPEN)) {
+            chooseFile(callbackContext);
+            return true;
+        }else if(action.equals(ACTION_IMAGE)){
+            chooseImage(callbackContext);
+            return true;
+        }
+
+        return false;
+    }
+
+    public void chooseImage(CallbackContext callbackContext){
+        /*Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.addCategory(Intent.ACTION_GET_CONTENT);*/
+        Intent intent_gallery = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        //Intent chooser = Intent.createChooser(intent_gallery, "Select File");
+        cordova.startActivityForResult(this, intent_gallery, PICK_FILE_REQUEST);
+
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+        pluginResult.setKeepCallback(true);
+        callback = callbackContext;
+        callbackContext.sendPluginResult(pluginResult);
+    }
+    public void chooseFile(CallbackContext callbackContext) {
+
+        // type and title should be configurable
+
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+
+        Intent chooser = Intent.createChooser(intent, "Select File");
+        cordova.startActivityForResult(this, chooser, PICK_FILE_REQUEST);
+
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+        pluginResult.setKeepCallback(true);
+        callback = callbackContext;
+        callbackContext.sendPluginResult(pluginResult);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == PICK_FILE_REQUEST && callback != null) {
+
+            if (resultCode == Activity.RESULT_OK) {
+
+                Uri uri = data.getData();
+
+                if (uri != null) {
+
+                    Log.w(TAG, uri.toString());
+                    callback.success(uri.toString());
+
+                } else {
+
+                    callback.error("File uri was null");
+
+                }
+
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+
+                // TODO NO_RESULT or error callback?
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+                callback.sendPluginResult(pluginResult);
+
+            } else {
+
+                callback.error(resultCode);
+            }
+        }
+    }
+}
---
+++ www/fileChooser.js
@@ -0,0 +1,19 @@
+cordova.define("com.megster.cordova.FileChooser.FileChooser", function(require, exports, module) {
+
+var exec = require("cordova/exec");
+
+var FileChooser = function () {
+  this.name = "FileChooser";
+};
+
+FileChooser.prototype.open = function (success, failure) {
+                                     exec(success, failure, "FileChooser", "open", []);
+                                 };
+
+FileChooser.prototype.chooseImage = function (success, failure) {
+                                     exec(success, failure, "FileChooser", "image", []);
+                                 };
+
+module.exports = new FileChooser();
+
+});
